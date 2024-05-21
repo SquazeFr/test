b15c7aba4afa75dfc8da9a4854e6e139ead245c9
SquazeFr <148233579+SquazeFr@users.noreply.github.com>
2024-05-22 01:01:09 +0200

    test

---
+++ src/screens/HomePage/HomePage.jsx
@@ -0,0 +1,192 @@
+import React from "react";
+import "./style.css";
+
+export const HomePage = () => {
+  return (
+    <div className="home-page">
+      <div className="div">
+        <div className="a-propos">
+          <div className="overlap">
+            <div className="text-wrapper">À Porpos</div>
+            <div className="text-wrapper-2">À Propos</div>
+            <div className="contenue">
+              <p className="je-m-appel-gabriel">
+                <span className="span">Je m’appel </span>
+                <span className="text-wrapper-3">Gabriel Bisson--Barret</span>
+              </p>
+              <div className="overlap-group">
+                <div className="text-wrapper-4">Prénom</div>
+                <div className="text-wrapper-5">Gabriel Bisson--Barret</div>
+              </div>
+              <div className="overlap-2">
+                <div className="text-wrapper-6">Téléphone</div>
+                <p className="p">01 23 45 67 89</p>
+              </div>
+              <div className="overlap-group-2">
+                <div className="text-wrapper-7">Àge</div>
+                <div className="text-wrapper-8">18 ans</div>
+              </div>
+              <div className="overlap-3">
+                <div className="text-wrapper-9">Mail</div>
+                <div className="text-wrapper-10">cestmonmail@gmail.com</div>
+              </div>
+              <div className="overlap-4">
+                <div className="text-wrapper-11">Études</div>
+                <p className="text-wrapper-12">Lycée Gustave Eiffel : STI2D SIN (2020 -2023)</p>
+                <p className="text-wrapper-13">IUT Bordeaux Montaigne : MMI (2024 -2026)</p>
+              </div>
+              <p className="text-wrapper-14">
+                Je suis étudiant en 1ère année de formation supérieur à l’IUT Bordeaux Montaigne dans a filière MMI
+                (Métiers du Multimédia et de l’Internet). Depuis 2019 je me spécialise dans le design graphique de logo,
+                chartes graphiques ainsi que dans la création et refonte de site web.
+              </p>
+              <div className="overlap-5">
+                <div className="cadre" />
+                <img className="ma-photo" alt="Ma photo" src="/img/ma-photo.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="je-suis">
+          <div className="overlap-6">
+            <div className="text-wrapper-15">Je suis</div>
+            <div className="text-wrapper-16">GABRIEL</div>
+            <div className="text-wrapper-17">BISSON--BARRET</div>
+            <div className="cv-bouton">
+              <div className="overlap-group-3">
+                <div className="boutton-CV" />
+                <div className="text-wrapper-18">Télécharger mon CV</div>
+              </div>
+            </div>
+            <div className="carr-dco" />
+          </div>
+        </div>
+        <div className="overlap-7">
+          <div className="skills-home-page">
+            <div className="overlap-8">
+              <div className="overlap-9">
+                <div className="text-wrapper-19">Skills</div>
+                <div className="text-wrapper-20">Skills</div>
+                <div className="photoshop">
+                  <div className="rectangle-wrapper">
+                    <div className="rectangle" />
+                  </div>
+                  <div className="text-wrapper-21">Adobe Photoshop</div>
+                  <div className="text-wrapper-22">80%</div>
+                </div>
+                <div className="premierepro">
+                  <div className="rectangle-wrapper">
+                    <div className="rectangle-2" />
+                  </div>
+                  <div className="text-wrapper-23">Adobe Première Pro</div>
+                  <div className="text-wrapper-22">65%</div>
+                </div>
+              </div>
+              <div className="maquettage">
+                <div className="rectangle-wrapper">
+                  <div className="rectangle-3" />
+                </div>
+                <div className="text-wrapper-24">Maquettage de site</div>
+                <div className="text-wrapper-22">70%</div>
+              </div>
+              <div className="illustrator">
+                <div className="rectangle-wrapper">
+                  <div className="rectangle-4" />
+                </div>
+                <div className="text-wrapper-25">Adobe Illustrator</div>
+                <div className="text-wrapper-22">50%</div>
+              </div>
+              <div className="after-effect">
+                <div className="rectangle-wrapper">
+                  <div className="rectangle-5" />
+                </div>
+                <div className="text-wrapper-26">Anglais</div>
+                <div className="text-wrapper-22">90%</div>
+              </div>
+              <div className="skills">
+                <div className="rectangle-wrapper">
+                  <div className="rectangle-2" />
+                </div>
+                <div className="text-wrapper-27">Adobe After Effect</div>
+                <div className="text-wrapper-22">65%</div>
+              </div>
+              <div className="figma">
+                <div className="rectangle-wrapper">
+                  <div className="rectangle-6" />
+                </div>
+                <div className="text-wrapper-28">FIGMA</div>
+                <div className="text-wrapper-22">75%</div>
+              </div>
+              <div className="html-css">
+                <div className="rectangle-wrapper">
+                  <div className="rectangle-7" />
+                </div>
+                <div className="text-wrapper-29">HTML/CSS</div>
+                <div className="text-wrapper-22">60%</div>
+              </div>
+              <div className="text-wrapper-30">ET BIEN PLUS ENCORE...</div>
+            </div>
+          </div>
+          <div className="portfolio-home-page">
+            <div className="overlap-10">
+              <div className="portfolio" />
+              <div className="text-wrapper-31">Portfolio</div>
+              <div className="text-wrapper-32">Portfolio</div>
+              <div className="portfolio-logo">
+                <div className="overlap-group-4">
+                  <img className="logo-vv" alt="Logo vv" src="/img/logo-vv.png" />
+                  <div className="logo-case" />
+                </div>
+                <div className="text-wrapper-33">LOGO</div>
+              </div>
+              <div className="groupe-site">
+                <div className="text-wrapper-34">SITE WEB</div>
+                <img className="site-web-case" alt="Site web case" src="/img/site-web-case.png" />
+              </div>
+              <img className="groupe-vido" alt="Groupe vido" src="/img/groupe-vid-o.png" />
+            </div>
+          </div>
+          <footer className="footer">
+            <div className="overlap-11">
+              <div className="footer-2" />
+              <div className="text-wrapper-35">Me contacter</div>
+              <div className="text-footer">
+                <img className="icon-phone" alt="Icon phone" src="/img/icon-phone.png" />
+                <div className="overlap-12">
+                  <img className="icon-mail" alt="Icon mail" src="/img/icon-mail.png" />
+                  <div className="text-wrapper-36">PAR MAIL</div>
+                  <div className="text-wrapper-37">cestmonmail@gmail.com</div>
+                </div>
+                <div className="overlap-group-5">
+                  <img className="icon-linkedin" alt="Icon linkedin" src="/img/icon-linkedin.png" />
+                  <div className="text-wrapper-38">LINKEDIN</div>
+                  <a
+                    className="text-wrapper-39"
+                    href="https://www.linkedin.com/in/gabriel-bisson-barret-4787a2293/overlay/about-this-profile/"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    Gabriel Bisson--Barret
+                  </a>
+                </div>
+                <div className="overlap-13">
+                  <div className="text-wrapper-40">PAR TÉLÉPHONE</div>
+                  <p className="text-wrapper-41">01 23 45 67 89</p>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </div>
+        <header className="header">
+          <img className="bouton-logo" alt="Bouton logo" src="/img/bouton-logo.png" />
+          <div className="navbar">
+            <div className="text-wrapper-42">CONTACT</div>
+            <div className="text-wrapper-43">PORTFOLIO</div>
+            <div className="text-wrapper-44">SKILLS</div>
+            <div className="text-wrapper-45">À PROPOS</div>
+          </div>
+        </header>
+      </div>
+    </div>
+  );
+};
